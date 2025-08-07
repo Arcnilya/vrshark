f9f790dc57e7cc33a992858e2e66aa57c34f43bc
Arcnilya <jonathan.magnusson@kau.se>
2025-08-07 17:15:04 +0200

    Added laser to right hand controller as well

--- scripts/right_hand.gd
+++ scripts/right_hand.gd
@@ -2,13 +2,15 @@ extends XRController3D
 
 signal ax_button_pressed
 signal by_button_pressed
+var show_laser = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	self.button_pressed.connect(_on_button_pressed)
+	self.button_released.connect(_on_button_released)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	$FunctionPointer/Laser.visible = show_laser
 	
 func _on_button_pressed(button_name: String) -> void:
 	if not "touch" in button_name:
@@ -17,3 +19,9 @@ func _on_button_pressed(button_name: String) -> void:
 		emit_signal("ax_button_pressed")
 	if button_name == "by_button":
 		emit_signal("by_button_pressed")
+	if button_name == "grip_click":
+		show_laser = true
+
+func _on_button_released(button_name: String) -> void:
+	if button_name == "grip_click":
+		show_laser = false
